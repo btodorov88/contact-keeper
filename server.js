786ba023baab32755d36ae67ede2507fbc984257
Borislav Todorov <btodorov@axway.com>
2020-03-06 11:25:01 +0200

    heroku deployment

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const connectDb = require("./config/db");
+const path = require("path");
 
 const app = express();
 
 connectDb();
 
 app.use(express.json({ extended: false }));
-
 
 const PORT = process.env.PORT || 5000;
 
@@ -15,5 +15,12 @@ app.get("/", (req, res) => res.json({ message: "Welkome to the API" }));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
